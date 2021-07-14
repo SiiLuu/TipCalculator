27121d59cafd68d10bb8b7faf4132b540db3b6fa
lucas simao <lucas.simao@epitech.eu>
2021-07-14 20:58:48 +0200

    [ADD] init repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 
+import logo from "./images/logo.svg";
+
 function App() {
   return (
-    <div>
-      <p className="text-red-500">tamer</p>
+    <div className="flex flex-col w-full h-screen bg-LightGrayishCyan1">
+      <div className="flex flex-col m-auto">
+        <div className="m-auto -mt-10">
+          <img src={logo} className="" alt="logo" />
+        </div>
+        <div
+          style={{ width: "64vw", height: "50vh" }}
+          className="flex m-auto mt-10 bg-White rounded-3xl"
+        >
+          <div className="w-1/2 h-full p-7">
+            <div className="flex flex-col w-full h-full"></div>
+          </div>
+          <div className="w-1/2 h-full p-7">
+            <div className="flex flex-col w-full h-full bg-VeryDarkCyan rounded-3xl p-10">
+              <div className="flex flex-col w-full h-full">
+                <div></div>
+                <div></div>
+                <button className="mb-0 mt-auto w-full py-3 bg-StrongCyan text-lg font-extrabold rounded-lg">
+                  RESET
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        StrongCyan: 'hsl(172, 67%, 45%)',
+        VeryDarkCyan: 'hsl(183, 100%, 15%)',
+        DarkGrayishCyan1: 'hsl(186, 14%, 43%)',
+        DarkGrayishCyan2: 'hsl(184, 14%, 56%)',
+        LightGrayishCyan1: 'hsl(185, 41%, 84%)',
+        LightGrayishCyan2: 'hsl(189, 41%, 97%)',
+        White: 'hsl(0, 0%, 100%)'
+      }
+    },
   },
   variants: {
     extend: {},
